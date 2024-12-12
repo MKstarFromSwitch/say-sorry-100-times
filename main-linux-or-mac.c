062f9e4b026b4a682bb84c5d91b804decefb7321
MKstar <mkstarfromswitch@gmail.com>
2024-12-13 00:25:44 +0300

    Added 2 fixed C files for different OSes

---
+++ main-linux-or-mac.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int i = 1;
+    system("clear");
+    while (i < 100) {
+        printf("sorry\n");
+        i++;
+    }
+}
---
+++ main-windows.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int i = 1;
+    system("cls");
+    while (i < 100) {
+        printf("sorry\n");
+        i++;
+    }
+}
